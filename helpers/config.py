b98c9df8280d21b166e52c874e726653e56b40c5
Sunpy <sunpy@osufx.com>
2018-02-13 14:09:45 +0100

    Added relax config field

--- helpers/config.py
+++ helpers/config.py
@@ -60,6 +60,7 @@ class config:
 			self.config.get("server", "banchourl")
 			self.config.get("server", "threads")
 			self.config.get("server", "apikey")
+			self.config.get("server", "relax")
 
 			self.config.get("sentry","enable")
 			self.config.get("sentry","dns")
@@ -112,6 +113,7 @@ class config:
 		self.config.set("server", "banchourl", "http://127.0.0.1:5001")
 		self.config.set("server", "threads", "16")
 		self.config.set("server", "apikey", "changeme")
+		self.config.set("server", "relax", "0")
 
 		self.config.add_section("sentry")
 		self.config.set("sentry", "enable", "False")
