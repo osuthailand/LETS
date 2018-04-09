fa0ff1ae107a37182dda5c95bd81101a4f40c790
Sunpy <sunpy@osufx.com>
2018-04-09 08:24:11 +0200

    Added osu-error.php handling

--- handlers/osuErrorHandler.py
+++ handlers/osuErrorHandler.py
@@ -1,11 +1,39 @@
+import os
+
 import tornado.gen
 import tornado.web
+import json
 
 from common.web import requestsManager
+from common.log import logUtils as log
+from constants import exceptions
+from common import generalUtils
 
-
+MODULE_NAME = "clienterror"
+REQUIRED_DATA = ["u", "osumode", "gamemode", "gametime", "audiotime", "culture", "b", "bc", "exception", "feedback", "stacktrace", "iltrace", "version", "exehash", "config"]
 class handler(requestsManager.asyncRequestHandler):
 	@tornado.web.asynchronous
 	@tornado.gen.engine
-	def asyncGet(self):
-		self.write("")
+	def asyncPost(self):
+		try:
+			if not requestsManager.checkArguments(self.request.arguments, REQUIRED_DATA):
+				raise exceptions.invalidArgumentsException(MODULE_NAME)
+
+			# Get a random error id
+			found = False
+			errorID = ""
+			while not found:
+				errorID = generalUtils.randomString(8)
+				if not os.path.isfile(".data/clienterrors/{}.json".format(errorID)):
+					found = True
+			
+			# Write error file to .data folder
+			with open(".data/clienterrors/{}.json".format(errorID), "wb") as f:
+				f.write(json.dumps(self.request.arguments))
+
+			# Output
+			log.info("New client-error from {}:{} ({})".format(self.get_argument("u"), self.get_argument("i"), errorID))
+
+			self.write("")
+		except exceptions.invalidArgumentsException:
+			pass
