a270413f762ca20f90565e9ed7eebd7b0229caff
Aoba Suzukaze <neko.aoba.suzukaze@gmail.com>
2019-10-25 20:57:46 +0700

    Remove unused function and add Relax board

--- handlers/getScoresHandler.pyx
+++ handlers/getScoresHandler.pyx
@@ -4,6 +4,8 @@ import tornado.web
 
 from objects import beatmap
 from objects import scoreboard
+from objects import scoreboardRelax
+from objects import scoreboardRelaxScore
 from common.constants import privileges
 from common.log import logUtils as log
 from common.ripple import userUtils
@@ -72,6 +74,8 @@ class handler(requestsManager.asyncRequestHandler):
 			country = False
 			friends = False
 			modsFilter = -1
+			mods = int(self.get_argument("mods")
+
 			if scoreboardType == 4:
 				# Country leaderboard
 				country = True
@@ -79,9 +83,6 @@ class handler(requestsManager.asyncRequestHandler):
 				# Mods leaderboard, replace mods (-1, every mod) with "mods" GET parameters
 				modsFilter = int(self.get_argument("mods"))
 
-				# Disable automod (pp sort) if we are not donors
-				if not isDonor:
-					modsFilter = modsFilter & ~mods.AUTOPLAY
 			elif scoreboardType == 3 and isDonor:
 				# Friends leaderboard
 				friends = True
@@ -94,26 +95,20 @@ class handler(requestsManager.asyncRequestHandler):
 			bmap = beatmap.beatmap(md5, beatmapSetID, gameMode)
 
 			# Create leaderboard object, link it to bmap and get all scores
-			sboard = scoreboard.scoreboard(
+			if bool(mods & 128):
+					sboard = scoreboardRelax.scoreboardRelax(
 					username, gameMode, bmap, setScores=True, country=country, mods=modsFilter, friends=friends
-			)
+					)
+			else:
+					sboard = scoreboard.scoreboard(
+					username, gameMode, bmap, setScores=True, country=country, mods=modsFilter, friends=friends
+					)
 
 			# Data to return
 			data = ""
 			data += bmap.getData(sboard.totalScores, scoreboardVersion)
 			data += sboard.getScoresData()
 			self.write(data)
-
-			# Send bancho notification if needed
-			if modsFilter > -1:
-				knowsPPLeaderboard = glob.redis.get("lets:knows_pp_leaderboard:{}".format(userID)) is not None
-				if modsFilter & mods.AUTOPLAY > 0 and not knowsPPLeaderboard:
-					glob.redis.set("lets:knows_pp_leaderboard:{}".format(userID), "1", 1800)
-					glob.redis.publish("peppy:notification", json.dumps({
-						"userID": userID,
-						"message": "Hi there! Scores are now sorted by PP. You can change scores sort mode by toggling the 'Auto' mod and filtering the leaderboard by Active mods. Note that this option is available only for donors and we don't recommend saving replays when the leaderboard is sorted by pp, due to some client limitations."
-					}))
-
 
 			# Datadog stats
 			glob.dog.increment(glob.DATADOG_PREFIX+".served_leaderboards")
