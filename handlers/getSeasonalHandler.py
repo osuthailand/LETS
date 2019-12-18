97c062b3cab735cbf60666d72ab85cdee591f05d
Aoba Suzukaze <neko.aoba.suzukaze@gmail.com>
2019-12-18 21:46:58 +0700

    I'm too lazy, let's try this

--- handlers/getSeasonalHandler.py
+++ handlers/getSeasonalHandler.py
@@ -13,23 +13,7 @@ class handler(requestsManager.asyncRequestHandler):
 	@tornado.gen.engine
 	def asyncGet(self):
 		try:
-			args = {}
-			#if "stream" in self.request.arguments:
-			#	args["stream"] = self.get_argument("stream")
-			#if "action" in self.request.arguments:
-			#	args["action"] = self.get_argument("action")
-			#if "time" in self.request.arguments:
-			#	args["time"] = self.get_argument("time")
-
-			# Pass all arguments otherwise it doesn't work
-			for key, _ in self.request.arguments.items():
-				args[key] = self.get_argument(key)
-
-			if args["action"].lower() == "put":
-				self.write("nope")
-				return
-
-			response = requests.get("https://osu.ppy.sh/web/osu-getseasonal.php?{}".format(urlencode(args)))
+			response = requests.get("https://osu.ppy.sh/web/osu-getseasonal.php")
 			self.write(response.text)
 		except Exception as e:
 			log.error("check-seasonal failed: {}".format(e))
