aa8ed2a863ba8280ba5457a62bf36cceb55a8d49
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-01-02 09:56:18 +0700

    Baka...

--- objects/score.pyx
+++ objects/score.pyx
@@ -282,6 +282,7 @@ class score:
 					self.completed = 3
 					self.rankedScoreIncrease = self.score
 					self.oldPersonalBest = 0
+					self.personalOldBestScore = None
 				else:
 					# Set old personal best and calculates PP
 					self.personalOldBestScore = personalBest["id"]
