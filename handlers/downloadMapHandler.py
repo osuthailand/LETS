1bf4d08fe15508dd656f8fb58fa78448dee25649
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-02-20 20:43:14 +0000

    Aoba coding
    Yes, i tried.

--- handlers/downloadMapHandler.py
+++ handlers/downloadMapHandler.py
@@ -3,6 +3,7 @@ import json
 
 import tornado.gen
 import tornado.web
+import re
 
 from common.web import requestsManager
 from common.sentry import sentry
@@ -23,18 +24,22 @@ class handler(requestsManager.asyncRequestHandler):
 				bid = bid[:-1]
 			bid = int(bid)
 
-			self.set_status(302, "Moved Temporarily")
+			self.set_status(200, "OK")
 			if glob.conf.config["beatconnect"]["enable"]:
 				uniqueid = None
 				beatmap = requests.get("https://beatconnect.io/api/beatmap/{}/?token={}".format(bid, glob.conf.config["beatconnect"]["apikey"])).text
 				if beatmap != "null\n":
 					uniqueid = json.loads(beatmap)['unique_id']
 				url = "https://beatconnect.io/b/{}/{}{}".format(bid, uniqueid, "?novideo=1" if noVideo else "")
+				response = requests.get(url)
 			else:
 				url = "https://pisstau.be/d/{}{}".format(bid, "?novideo" if noVideo else "")
-			self.add_header("Location", url)
+			self.add_header("Content-Type", "application/octet-stream")
+			self.add_header("Content-Length", response.headers['Content-Length'])
+			self.add_header("Content-Disposition", response.headers['Content-Disposition'])
 			self.add_header("Cache-Control", "no-cache")
 			self.add_header("Pragma", "no-cache")
+			self.write(response.content)
 		except ValueError:
 			self.set_status(400)
 			self.write("Invalid set id")
