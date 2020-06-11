97bc37e0bcf8d7b4139a236152a428384dd0a583
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-06-12 00:41:55 +0700

    More Info

--- helpers/kotrikhelper.py
+++ helpers/kotrikhelper.py
@@ -7,6 +7,24 @@ def verifyScoreData(scoreData, securityHash, sbk=None):
     # Full nonHashedString
     # Taken from osu! v20200519.1
     # chickenmcnuggets{0}o15{1}{2}smustard{3}{4}uu{5}{6}{7}{8}{9}{10}{11}Q{12}{13}{15}{14:yyMMddHHmmss}{16}{17}
+    # {0} = Count100 + Count300
+    # {1} = Count50
+    # {2} = CountGeki
+    # {3} = CountKatu
+    # {4} = CountMiss
+    # {5} = FileChecksum
+    # {6} = MaxCombo
+    # {7} = Perfect
+    # {8} = PlayerName
+    # {9} = TotalScore
+    # {10} = Rank
+    # {11} = (int)(Mods)EnabledMods
+    # {12} = Pass
+    # {13} = (int)PlayMode
+    # {14} = Date.ToUniversalTime()
+    # {15} = General.VERSION
+    # {16} = GameBase.ClientHash
+    # {17} = Beatmap.StoryboardHash
     nonHashedString = "chickenmcnuggets{}o15{}{}smustard{}{}uu{}{}{}{}{}{}{}Q{}{}{}{}{}".format(
         int(scoreData[4])+int(scoreData[3]),
         int(scoreData[5]),
