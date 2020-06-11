72fb900644569d7a4664348c81f76ce9e014418b
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-06-11 12:45:12 +0300

    smh, all storage dead. we will make one then

--- handlers/downloadMapHandler.py
+++ handlers/downloadMapHandler.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+import random
+
 import tornado.gen
 import tornado.web
 
@@ -18,43 +20,16 @@ class handler(requestsManager.asyncRequestHandler):
 	@tornado.gen.engine
 	@sentry.captureTornado
 	def asyncGet(self, bid):
-		if glob.conf.config["beatconnect"]["enable"] == True:
-			try:
-				noVideo = bid.endswith("n")
-				if noVideo:
-					bid = bid[:-1]
-				bid = int(bid)
+		try:
+			noVideo = bid.endswith("n")
+			if noVideo:
+				bid = bid[:-1]
+			bid = int(bid)
 
-				self.set_status(200, "OK")
-				beatmap = requests.get("https://beatconnect.io/api/beatmap/{}/?token={}".format(bid, glob.conf.config["beatconnect"]["apikey"])).text
-				uniqueid = json.loads(beatmap)['unique_id']
-				url = "https://beatconnect.io/b/{}/{}{}".format(bid, uniqueid, "?novideo=1" if noVideo else "")
-				response = requests.get(url)
-				self.add_header("Content-Type", "application/octet-stream")
-				self.add_header("Content-Length", response.headers['Content-Length'])
-				self.add_header("Content-Disposition", response.headers['Content-Disposition'])
-				self.add_header("Cache-Control", "no-cache")
-				self.add_header("Pragma", "no-cache")
-				self.write(response.content)
-				#log.info("USING beatconnect.io FOR BEATMAPS")
-			except ValueError:
-				self.set_status(400)
-				self.write("Invalid set id")
-		else:
-			try:
-				noVideo = bid.endswith("n")
-				if noVideo:
-					bid = bid[:-1]
-				bid = int(bid)
-
-				self.set_status(302, "Moved Temporarily")
-				#URL CAN BE CHANGED TO ANYTHING
-				#SUCH AS https://akatsuki.pw/d/
-				url = "https://osu.gatari.pw/d/{}{}".format(bid, "?novideo" if noVideo else "")
-				self.add_header("Location", url)
-				self.add_header("Cache-Control", "no-cache")
-				self.add_header("Pragma", "no-cache")
-				#log.info("USING pisstau.be FOR BEATMAPS")
-			except ValueError:
-				self.set_status(400)
-				self.write("Invalid set id")
+			self.set_status(302, "Moved Temporarily")
+			self.add_header("Location", "https://storage.ainu.pw/d/{}{}".format(bid, "?novideo" if noVideo else ""))
+			self.add_header("Cache-Control", "no-cache")
+			self.add_header("Pragma", "no-cache")
+		except ValueError:
+			self.set_status(400)
+			self.write("Invalid set id")
